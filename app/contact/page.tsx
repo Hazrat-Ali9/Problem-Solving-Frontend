4cf591968222102d2c1c81c12395a7a0d0f6d54f
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-05 14:34:56 +0600

    content   :tada:

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import toast from "react-hot-toast";
-
+// Page tsx 
 const page = () => {
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
