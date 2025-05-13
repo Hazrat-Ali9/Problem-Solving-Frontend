6a195129ee5f65c6a5f3375d4bf827d100f549d9
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-13 20:20:48 +0600

    route  🚕

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
-
+// Route
 export async function POST(req: NextRequest) {
   try {
     const data = await req.json();
