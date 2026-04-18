c4ee43f0a0d34e03b7f51e95305da4aecd538f91
GearHead87 <57955583+GearHead87@users.noreply.github.com>
2026-04-18 16:38:14 +0600

    Update next.config.ts to set output to "standalone" :sparkles:

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 // next config 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
   },
