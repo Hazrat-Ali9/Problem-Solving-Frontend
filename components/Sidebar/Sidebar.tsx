5d2cc2753066758071314a04e6038859236d0960
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-04-27 09:08:48 +0600

    sidebar tsx :rocket:

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -77,7 +77,7 @@ const Sidebar = () => {
             />
           </a>
           <a
-            href="https://codeforces.com/profile/Hazrat__Ali9"
+            href="https://codeforces.com/profile/Hazrat-Ali-MIT"
             target="_blank"
             className="text-gray-600 hover:text-blue-600"
           >
